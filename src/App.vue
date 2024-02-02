7c419b1d541015bb88e51231c3b39ae04ece2f55
GiuliaZd <yuliya.zd@gmail.com>
2024-02-02 18:28:16 +0200

    first commit for Heppaseikkailu

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Navbar />
+  <main class="container" style="margin-top: 70px">
+    <h1>Poniralli</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Navbar from '@/components/Navbar.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: { Navbar }
+});
+</script>
+<style></style>
