aef4cb93f11cac81e1ebd5b1c0c7feb8360c3870
GiuliaZd <yuliya.zd@gmail.com>
2024-02-04 20:23:20 +0200

    views and models were added, testing

---
+++ src/views/Kisat.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- eslint-disable-next-line vue/require-v-for-key -->
+    <div v-for="race in races">
+      <h2>{{ race.nimi }}</h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import KisaMalli from '../models/KisaMalli.ts';
+
+export default defineComponent({
+  name: 'Kisat',
+  setup() {
+    const races = ref<Array<KisaMalli>>([{ name: 'Vermo' }, { name: 'Killeri' }]);
+    return { races };
+  }
+});
+</script>
+
+<style></style>
